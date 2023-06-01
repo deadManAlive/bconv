0be95903ecef4b329df0113f19df85eb14621aa0
deadManAlive <alfarrakhan@gmail.com>
2023-06-01 14:32:56 +0700

    add height load and change message post

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,35 @@
 <script>
+    import { onMount } from "svelte";
+
     import Float32 from "./Float32.svelte";
+
+    let main;
+
+    onMount(async() => {
+        const height = main.clientHeight;
+
+        window.parent.postMessage(
+            {
+                bconvHeight: height,
+            },
+            "*"
+        );
+    });
+
+    function resizeCallback() {
+        const height = main.clientHeight;
+
+        window.parent.postMessage(
+            {
+                bconvHeight: height,
+            },
+            "*"
+        );
+    }
 </script>
 
-<Float32 />
+<svelte:window on:resize={resizeCallback} />
+
+<main bind:this={main}>
+    <Float32 />
+</main>
